6c3eb11b268eb64daa7f628b84cffe6cc10c900a
Prakhar Rajpali <rajpaliprakhar23@gmail.com>
2023-09-13 18:50:43 +0530

    Redirecting admmin pages

--- routes/admin/MorePeople.route.js
+++ routes/admin/MorePeople.route.js
@@ -5,7 +5,13 @@ const { isLoggedIn } = require("../../middleware/Auth.middleware");
 
 const router = express.Router();
 
-router.get("/", isLoggedIn, Controller.view);
+router.get("/:something", isLoggedIn, (req, res) => {
+	res.redirect("/admin/people");
+});
+// router.get("/", isLoggedIn, Controller.view);
+router.get("/", isLoggedIn, (req, res) => {
+	res.redirect("/admin/people");
+});
 router.get("/add", isLoggedIn, Controller.add);
 router.post("/", isLoggedIn, Controller.post);
 router.delete("/:id", isLoggedIn, Controller.remove);
